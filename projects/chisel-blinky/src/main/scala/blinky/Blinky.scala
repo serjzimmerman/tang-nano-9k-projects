2ed5e7dddeaa993ddb6b25c7501b6962c57ed975
Sergei Zimmerman <sstsimmerman@gmail.com>
2024-04-09 17:16:07 +0300

    feat(chisel-blinky): rework code, simple clock prescaler
    Implement basic clock prescaler and shift register with
    chisel.

--- projects/chisel-blinky/src/main/scala/blinky/Blinky.scala
+++ projects/chisel-blinky/src/main/scala/blinky/Blinky.scala
@@ -4,22 +4,57 @@ import chisel3._
 import chisel3.util.Counter
 import circt.stage.ChiselStage
 
-class Blinky(freq: Int, bits: Int) extends Module {
-  val io = IO(new Bundle {
-    val led = Output(UInt(bits.W))
-  })
-  val led = RegInit(1.U(bits.W))
-  val (_, counterWrap) = Counter(true.B, freq / 2)
-  when(counterWrap) {
-    led := (led << 1.U) | led(bits - 1)
+object ClockDivider {
+  def apply(
+      clockIn: Clock,
+      divideBy: Int
+  ): Clock = {
+    require(divideBy % 2 == 0, "Must divide by an even factor")
+
+    val outputClock = Wire(Clock())
+    withClock(clock = clockIn) {
+      val dividedClock = RegInit(false.B)
+      val max: Int = divideBy / 2
+      val (_, counterWrap) = Counter(true.B, max)
+      when(counterWrap) {
+        dividedClock := ~dividedClock
+      }
+      outputClock := dividedClock.asClock
+    }
+
+    outputClock
   }
+}
+
+class BlinkyIO(bits: Int) extends Bundle {
+  val led = Output(UInt(bits.W))
+}
+
+class Blinky(bits: Int) extends Module {
+  val io = IO(new BlinkyIO(bits))
+  val led = RegInit(1.U(bits.W))
+
+  led := (led << 1.U) | led(bits - 1)
   io.led := ~led
+}
+
+// https://stackoverflow.com/questions/76521229/how-to-deal-with-activelow-reset-change-implicit-clock-frequency
+class TopWrapper(freq: Int, bits: Int) extends Module {
+  val io = IO(new BlinkyIO(bits))
+  val invertedReset = ~reset.asBool
+  val dividedClock = withReset(reset = invertedReset) {
+    ClockDivider(clock, freq)
+  }
+  val top = withClockAndReset(clock = dividedClock, reset = invertedReset) {
+    Module(new Blinky(bits))
+  }
+  io <> top.io
 }
 
 object Main extends App {
   println(
     ChiselStage.emitSystemVerilog(
-      new Blinky(10000000, 6),
+      new TopWrapper(13_500_000, 6),
       firtoolOpts = Array(
         "-disable-all-randomization",
         "-strip-debug-info",

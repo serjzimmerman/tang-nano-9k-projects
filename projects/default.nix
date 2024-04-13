dcc8aee632f1c024ab9d5413faac83cc4e6c32b8
Sergei Zimmerman <52955378+serjzimmerman@users.noreply.github.com>
2024-04-13 18:53:47 +0300

    ci: add build-all job

--- projects/default.nix
+++ projects/default.nix
@@ -4,5 +4,6 @@
     blinky = pkgs.callPackage ./blinky { };
     blinkygen-chisel = pkgs.callPackage ./chisel-blinky { };
     chisel-blinky = pkgs.callPackage ./chisel-blinky { };
+    chisel-practice = pkgs.callPackage ./chisel-practice { };
   };
 }
